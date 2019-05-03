75fdf5840b1dcf09134390e66d005eb49129568e
Andrew Hager <scott.hager121@gmail.com>
2019-05-03 00:50:22 -0400

    it begins

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import hamburger from "./images/hamburger.png"
+import settings from "./images/settings.png"
+import addButton from "./images/add.png"
+import editButton from "./images/edit.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <img src={hamburger} id="hamburger" alt="hamburger"></img>
+        <h1>
+          ToDo App
+        </h1>
+        <img src={settings} id="settings" alt="settings"></img>
+      </div>
+      <div className="toDoList">
+        <h1>To Do</h1>
+        <div class="listBox">
+          <ul className="listItems">
+            <input type="checkbox"></input><li>Wake Up</li>
+          </ul>
+        </div>
+        <div class="listBox">
+          <ul className="listItems">
+            <input type="checkbox"></input><li>Brush Hair</li>
+          </ul>
+        </div>
+        <div class="listBox">
+          <ul className="listItems">
+            <input type="checkbox"></input><li>Brush Teeth</li>
+          </ul>
+        </div>
+        <div class="listBox">
+          <ul className="listItems">
+            <input type="checkbox"></input><li>Eat Breakfast</li>
+          </ul>
+        </div>
+      </div>
+      <footer>
+        <div class="addButtonElements">
+          <img src={addButton} id="add" alt="add"></img>
+          <h3>Add</h3>
+        </div>
+        <div class="editButtonElements">
+          <img src={editButton} id="edit" alt="edit"></img>
+          <h3>Edit</h3>
+        </div>
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
